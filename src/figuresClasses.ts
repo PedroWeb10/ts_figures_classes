5b09c7861a40dc32ddbacb34f95eb2f65c150e70
PedroWeb10 <pdro_dev@yahoo.com>
2025-05-13 10:53:12 -0300

    figures Classaes ts 2.1

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -51,7 +51,7 @@ export class Circle implements Figure {
   getArea(): number {
     const area = Math.PI * Math.pow(this.radius, 2);
 
-    return parseFloat(area.toFixed(2));
+    return Math.floor(area * 100) / 100;
   }
 }
 
